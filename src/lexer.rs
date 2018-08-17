0886a67294c270f0e3e2ff30b8896c6ae724fda9
shhr1214 <souhei0079@gmail.com>
2018-08-18 02:03:59 +0900

    add: lexer にテストを追加

--- src/lexer.rs
+++ src/lexer.rs
@@ -17,4 +17,80 @@ impl Lexer {
             ch: '0',
         }
     }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = 0;
+        } else {
+            self.ch = self.input.char().nth(self.read_position);
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use token;
+
+    #[test]
+    fn test_next_token() {
+        let input = r#"let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+"#;
+
+        let tests = vec![
+            (token.LET, "let"),
+            (token.IDENT, "five"),
+            (token.ASSIGN, "="),
+            (token.INT, "5"),
+            (token.SEMICOLON, ";"),
+            (token.LET, "let"),
+            (token.IDENT, "ten"),
+            (token.ASSIGN, "="),
+            (token.INT, "10"),
+            (token.SEMICOLON, ";"),
+            (token.LET, "let"),
+            (token.IDENT, "add"),
+            (token.ASSIGN, "="),
+            (token.FUNCTION, "fn"),
+            (token.LPAREN, "("),
+            (token.IDENT, "x"),
+            (token.COMMA, ","),
+            (token.IDENT, "y"),
+            (token.RPAREN, ")"),
+            (token.LBRACE, "{"),
+            (token.IDENT, "x"),
+            (token.PLUS, "+"),
+            (token.IDENT, "y"),
+            (token.SEMICOLON, ";"),
+            (token.RBRACE, "}"),
+            (token.SEMICOLON, ";"),
+            (token.LET, "let"),
+            (token.IDENT, "result"),
+            (token.ASSIGN, "="),
+            (token.IDENT, "add"),
+            (token.LPAREN, "("),
+            (token.IDENT, "five"),
+            (token.COMMA, ","),
+            (token.IDENT, "ten"),
+            (token.RPAREN, ")"),
+            (token.SEMICOLON, ";"),
+            (token.EOF, ""),
+        ];
+
+        let l = Lexer::new(input);
+
+        for test in tests.iter() {
+            let token = l.next_token();
+            assert!(token.type(), tests.0)
+        }
+    }
 }
