04da1672d4c84457fc74d1fbaf52292bc0da61dc
shhr1214 <souhei0079@gmail.com>
2018-08-20 18:14:02 +0900

    add: 解析器で処理するトークンを追加するためのテスト

--- src/lexer.rs
+++ src/lexer.rs
@@ -131,6 +131,8 @@ let add = fn(x, y) {
 };
 
 let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 "#;
 
         let tests = vec![
@@ -169,6 +171,18 @@ let result = add(five, ten);
             (token::COMMA, ","),
             (token::IDENT, "ten"),
             (token::RPAREN, ")"),
+            (token::SEMICOLON, ";"),
+            (token::BANG, "!"),
+            (token::MINUS, "-"),
+            (token::SLASH, "/"),
+            (token::ASTERISK, "*"),
+            (token::INT, "5"),
+            (token::SEMICOLON, ";"),
+            (token::INT, "5"),
+            (token::LT, "<"),
+            (token::INT, "10"),
+            (token::GT, ">"),
+            (token::INT, "5"),
             (token::SEMICOLON, ";"),
             (token::EOF, ""),
         ];
