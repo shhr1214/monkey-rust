570e5f5245c91382d3be7c536283c49a74393815
shhr1214 <souhei0079@gmail.com>
2018-08-22 01:58:50 +0900

    modify: 変に buf とか用意しないでもかけた。
    PROMPT の最初に ">>" を出すことができない。
    きっと BufReader とか使っているのがだめなんだろうけど、
    いまいち理由がわからない。
    
    こういうとき気軽に聞ける人がいないのが残念。

--- src/repl.rs
+++ src/repl.rs
@@ -1,24 +1,17 @@
-use std::io::{BufReader, Read, Result, Write};
+use std::io::{BufRead, BufReader, Read, Result, Write};
 
 use lexer::Lexer;
 use token;
 
 const PROMPT: &str = ">> ";
 
-pub fn start<T: Read, U: Write>(input: T, output: U) -> Result<usize> {
+pub fn start<T: Read, U: Write>(input: T, output: U) {
     let mut bf = BufReader::new(input);
-    loop {
-        println!("{}", PROMPT);
-        let mut buf = vec![];
-        let n = bf.read(&mut buf)?;
-        if n == 0 {
-            return Ok(0);
-        }
-
-        let mut lexer = Lexer::new(String::from_utf8(buf).unwrap_or("".to_string()));
+    for line in bf.by_ref().lines() {
+        let mut lexer = Lexer::new(line.unwrap());
         loop {
             let token = lexer.next_token();
-            if token.token_type() != token::EOF {
+            if token.token_type() == token::EOF {
                 break;
             }
             println!("{:?}", token);
